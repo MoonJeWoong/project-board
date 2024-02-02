47df6aadbac740cfcf8c3152b110ab6e6a77f06b
MoonJeWoong <munjin0201@naver.com>
2024-02-02 18:15:22 +0900

    feat: 게시글, 댓글 객체 엔티티화

--- src/test/java/board/domain/ArticleTest.java
+++ src/test/java/board/domain/ArticleTest.java
@@ -2,7 +2,6 @@ package board.domain;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
@@ -14,7 +13,6 @@ class ArticleTest {
     @Test
     public void 게시글_객체_정상_생성_테스트() {
         // given & then
-        assertDoesNotThrow(() -> new Article(1L, "content", "hashtags",
-                LocalDateTime.now(), "creator", LocalDateTime.now(), "updator"));
+        assertDoesNotThrow(() -> Article.of("title", "content", "hashtags"));
     }
 }

a95feecb3f0e0a5610cd5b18720db9a22c1f4615
MoonJeWoong <munjin0201@naver.com>
2024-02-05 01:16:14 +0900

    feat: pathParameter 검색 기능 구현

--- src/main/java/board/repository/ArticleRepository.java
+++ src/main/java/board/repository/ArticleRepository.java
@@ -1,9 +1,31 @@
 package board.repository;
 
 import board.domain.Article;
+import board.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // 제네릭으로 선언해준 엔티티 타입의 모든 속성들에 대한 검색 기능을 제공해준다.
+        QuerydslBinderCustomizer<QArticle>  // 기본적으로 제공되는 검색 기능에 대해 커스터마이징이 가능하다.
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        // interface에 정의된 메서드를 오버라이딩 하므로 default 접근 제어자를 사용한다.
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
